e1bb35cefe274083926fa6a463906f96f5d1c488
Daniel Bogan <d@waferbaby.com>
2018-07-11 03:20:47 +1000

    Set the editor to neovim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,6 @@ end
 set fish_greeting
 
 set -g fish_user_paths "$HOME/.rbenv/shims" "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
-set -x EDITOR /usr/local/bin/subl
+set -x EDITOR /usr/local/bin/nvim
 
 rbenv rehash >/dev/null ^&1
