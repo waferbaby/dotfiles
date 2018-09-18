84796a86eb2ffba799eedcdaee3f7ea4a47f6756
Daniel Bogan <d@waferbaby.com>
2018-09-19 07:54:13 +1000

    Support opening a specific note

--- .config/fish/functions/notes.fish
+++ .config/fish/functions/notes.fish
@@ -1,6 +1,12 @@
-function notes 
+function notes -a 'note_name'
   set source_directory $PWD
   cd ~/Documents/Notes
-  nvim +":FZF"
+
+  if test -n "$note_name"
+    nvim +":Note $note_name"
+  else
+    nvim +":FZF"
+  end
+
   cd $source_directory
 end
