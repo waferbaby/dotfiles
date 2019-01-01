7a5eaab399e00876413f76020615a5ae5b4d25a0
Daniel Bogan <d@waferbaby.com>
2019-01-01 16:20:58 +1100

    Strip newlines from the output

--- fish/functions/gear.fish
+++ fish/functions/gear.fish
@@ -1,5 +1,5 @@
 function gear
   cd ~/Development/usesthis/gear
-  fzf --preview="cat {}" --preview-window=right:60%:wrap --bind "enter:execute-silent(basename {} | sed 's/.json//' | pbcopy)"
+  fzf --preview="cat {}" --preview-window=right:60%:wrap --bind "enter:execute-silent(basename {} | sed 's/.json//' | tr -d '\n' | pbcopy)"
   cd ~/Development/usesthis
 end
