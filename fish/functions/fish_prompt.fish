0147eabc17ccf1aa0cdb0bc47057af5ade66ebef
Daniel Bogan <d@waferbaby.com>
2019-05-08 11:50:07 +1000

    Rework the prompt, show the current branch at right

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,7 @@
-set fish_color_cwd ffffff
-set fish_color_command ffffff
-set fish_color_param ffffff
-set fish_color_user 9E5CED
-set fish_color_host EEEEEE --underline
+set fish_color_branch aaaaaa
+set fish_color_cwd 33ccff
+set fish_color_host eeeeee
+set fish_color_user 9e5ced
 
 function fish_prompt
   set last_status $status
@@ -12,9 +11,15 @@ function fish_prompt
   set_color normal
   printf '@'
   set_color $fish_color_host
-  printf '%s' (hostname|cut -d . -f 1)
+  printf '%s ' (hostname|cut -d . -f 1)
   set_color normal
   set_color $fish_color_cwd
-  printf ' %s' (prompt_pwd)
-  printf '%s ' (__fish_git_prompt)
+  printf '%s' (prompt_pwd)
+  set_color normal
+  printf ' > '
+end
+
+function fish_right_prompt
+  set_color $fish_color_branch
+  printf '%s' (__fish_git_prompt)
 end
