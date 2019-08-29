6e9f1370593436fe2a654bf36f400db3ee6a5986
Daniel Bogan <d@waferbaby.com>
2019-08-30 00:31:39 +1000

    Follow symlinks with ripgrep in FZF

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ set fish_greeting
 set PATH /usr/local/bin /usr/local/sbin $PATH
 
 set -x EDITOR /usr/local/bin/nvim
-set -x FZF_DEFAULT_COMMAND "rg --files --hidden --glob '!.git/*'"
+set -x FZF_DEFAULT_COMMAND "rg --files --follow --hidden --glob '!.git/*'"
 set -x FZF_DEFAULT_OPTS "--reverse"
 
 status --is-interactive; and source (rbenv init -|psub)
