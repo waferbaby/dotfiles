db1b533bf19b0d5c30cad7eb82e8909965806947
Daniel Bogan <d@waferbaby.com>
2020-02-10 03:28:16 -0800

    Re-add the tmux startup, add ssh-agent

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,12 @@ set -x FZF_DEFAULT_OPTS "--reverse"
 
 status --is-interactive; and source (rbenv init -|psub)
 
+eval (ssh-agent -c) >> /dev/null
+
+if test -z $TMUX
+  tmux new -A -s "default"
+end
+
 if test -d ~/.config/fish/configs
   for config_file in ~/.config/fish/configs/*
     source $config_file
