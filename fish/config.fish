b2d940c2fe3b2e55d78493b940d9f19575ef46f1
Daniel Bogan <d@waferbaby.com>
2022-08-02 14:18:58 +1000

    Remove bash, add the M1 homebrew bin/ path

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,15 @@
-if status is-interactive
-and not set -q TMUX
-  exec tmux new -A -s "default"
-end
-
 set fish_greeting
 
-set PATH /usr/local/bin/ ~/.rbenv/bin $PATH
+set PATH /usr/local/bin/ /opt/homebrew/bin/ ~/.rbenv/bin $PATH
 
 set -x EDITOR /usr/bin/nvim
 set -x FZF_DEFAULT_COMMAND "rg --files --follow --hidden --glob '!.git/*'"
 set -x FZF_DEFAULT_OPTS "--reverse"
 
-export SHELL=/bin/bash
+if status is-interactive
+and not set -q TMUX
+  exec tmux new -A -s "default"
+end
 
 if status is-interactive
   source (rbenv init -|psub)
