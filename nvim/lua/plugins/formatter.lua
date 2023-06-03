e3a9f0a739be335f1b44485b76ec4f97cfe75f2f
Daniel Bogan <d@waferbaby.com>
2023-06-03 07:11:47 +0000

    Switch to syntax_tree

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,12 +1,26 @@
 return {
   "mhartington/formatter.nvim",
   opts = function()
+    local util = require "formatter.util"
+
     return {
       logging = true,
       log_level = vim.log.levels.WARN,
       filetype = {
-        ruby = { require("formatter.filetypes.ruby").rubocop },
-        ["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace }
+        ["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
+        ruby = {
+          function()
+            return {
+              exe = "stree",
+              args = {
+                "format",
+                "--print-width=100",
+                util.escape_path(util.get_current_buffer_file_path())
+              },
+              stdin = true
+            }
+          end
+        }
       }
     }
   end
