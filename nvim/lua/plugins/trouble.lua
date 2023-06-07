caaf203214962b9c9d55b02f2f83b240a2d41534
Daniel Bogan <d@waferbaby.com>
2023-06-07 16:45:36 +1000

    Add the final boot config, the nvim-lint and trouble plugins

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    auto_open = true,
+    icons = false,
+    fold_open = "v",
+    fold_closed = ">",
+    indent_lines = false,
+    signs = {
+        error = "error",
+        warning = "warn",
+        hint = "hint",
+        information = "info"
+    },
+    use_diagnostic_signs = false
+  }
+}
