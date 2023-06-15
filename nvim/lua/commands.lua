bdf333b13ac66256283796eceacb8c5bf7b24989
Daniel Bogan <d@waferbaby.com>
2023-06-15 10:08:31 +1000

    Use q-args

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "silent! cd %:p:h" })
 vim.api.nvim_create_autocmd("BufWritePost", { pattern = "*", command = "FormatWrite" })
-vim.api.nvim_create_user_command('Rg', "silent! grep <f-args> | copen", { nargs = "*" })
+vim.api.nvim_create_user_command('Rg', "silent! grep <q-args> | copen", { nargs = "*" })
