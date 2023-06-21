4bb8a7d1559d5fc0deb5f5ba4b54df3ee25753a6
Daniel Bogan <d@waferbaby.com>
2023-06-21 16:55:51 +1000

    Set up the auto-installed libs, enable incremental selection

--- source/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ source/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,12 @@ return {
   "nvim-treesitter/nvim-treesitter",
   init = function()
     require("nvim-treesitter.configs").setup({
+      ensure_installed = { "css", "fish", "html", "javascript", "json", "lua", "markdown", "ruby", "sql" },
+      auto_install = true,
       highlight = {
+        enable = true
+      },
+      incremental_selection = {
         enable = true
       }
     })
