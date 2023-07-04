55c2d4268401fb79a39ea61c5c62c75a3af1420a
Daniel Bogan <d@waferbaby.com>
2023-07-04 12:35:11 +1000

    Bring Telescope keymaps here, including Space-V to bring up my vim config path

--- source/.config/nvim/lua/keymaps.lua
+++ source/.config/nvim/lua/keymaps.lua
@@ -1,11 +1,19 @@
 vim.g.mapleader = " "
 
+-- General
+
 vim.keymap.set("n", "<leader>/", ":%!sort -R<CR>")
 vim.keymap.set("n", "<leader>[", ":bp<cr>")
 vim.keymap.set("n", "<leader>]", ":bn<cr>")
 vim.keymap.set("n", "<leader>f", ":Rg ")
 vim.keymap.set("n", "<leader>j", "vipJ")
-vim.keymap.set("n", "<leader>v", ":edit $MYVIMRC<cr>")
+
+-- Telescope
+
+vim.keymap.set("n", "<leader>g", function() require('telescope.builtin').git_status{} end)
+vim.keymap.set("n", "<leader>t", function() require('telescope.builtin').find_files{} end)
+vim.keymap.set("n", "<leader>v", function() require('telescope.builtin').find_files{ cwd = "~/.config/nvim/" } end)
+vim.keymap.set("n", "<leader><leader>", function() require('telescope.builtin').buffers{} end)
 
 -- Navigator
 
