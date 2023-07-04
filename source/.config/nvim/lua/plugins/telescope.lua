a4997981f8981ed0d28dd16ee4688e8f1bd150d4
Daniel Bogan <d@waferbaby.com>
2023-07-04 10:27:11 +1000

    Remove the git filter from the find files call

--- source/.config/nvim/lua/plugins/telescope.lua
+++ source/.config/nvim/lua/plugins/telescope.lua
@@ -20,7 +20,7 @@ return {
       pickers = {
         find_files = {
           theme = "dropdown",
-          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+          find_command = { "rg", "--files", "--hidden" }
         },
         git_status = { theme = "dropdown" }
       }
