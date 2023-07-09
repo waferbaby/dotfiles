1c59b9c06ef0b9abae1e1acfd42682f09b53b720
Daniel Bogan <d@waferbaby.com>
2023-07-09 05:44:51 +0000

    Add the Lua formatter

--- source/.config/nvim/lua/plugins/formatter.lua
+++ source/.config/nvim/lua/plugins/formatter.lua
@@ -1,28 +1,29 @@
 return {
-  "mhartington/formatter.nvim",
-  opts = function()
-    local util = require "formatter.util"
+	"mhartington/formatter.nvim",
+	opts = function()
+		local util = require("formatter.util")
 
-    return {
-      logging = true,
-      log_level = vim.log.levels.WARN,
-      filetype = {
-        ["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
-        javascript = { require("formatter.filetypes.javascript").prettier },
-        ruby = {
-          function()
-            return {
-              exe = "stree",
-              args = {
-                "format",
-                "--print-width=100",
-                util.escape_path(util.get_current_buffer_file_path())
-              },
-              stdin = true
-            }
-          end
-        }
-      }
-    }
-  end
+		return {
+			logging = true,
+			log_level = vim.log.levels.WARN,
+			filetype = {
+				["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
+				javascript = { require("formatter.filetypes.javascript").prettier },
+				lua = { require("formatter.filetypes.lua").stylua },
+				ruby = {
+					function()
+						return {
+							exe = "stree",
+							args = {
+								"format",
+								"--print-width=100",
+								util.escape_path(util.get_current_buffer_file_path()),
+							},
+							stdin = true,
+						}
+					end,
+				},
+			},
+		}
+	end,
 }
