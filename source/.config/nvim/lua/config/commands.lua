026b1934cf9fed35bba1628e9ae66b7b460751f1
Daniel Bogan <d@waferbaby.com>
2023-08-04 13:28:24 +1000

    Add the lint autocommand

--- source/.config/nvim/lua/config/commands.lua
+++ source/.config/nvim/lua/config/commands.lua
@@ -1,3 +1,9 @@
 vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "silent! cd %:p:h" })
 vim.api.nvim_create_autocmd("BufWritePost", { pattern = "*", command = "FormatWrite" })
-vim.api.nvim_create_user_command('Rg', "silent! grep <q-args> | copen", { nargs = "*" })
+vim.api.nvim_create_autocmd("BufModifiedSet", {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
+
+vim.api.nvim_create_user_command("Rg", "silent! grep <q-args> | copen", { nargs = "*" })
