ee783d6cd3a677caccb090b6bf7ed221ef6935fa
Daniel Bogan <d@waferbaby.com>
2023-08-07 15:04:33 +1000

    Simplify the grepprg config, set the RIPGREP_CONFIG_PATH env

--- source/.config/fish/config.fish
+++ source/.config/fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting
 fish_add_path ~/.rbenv/shims /usr/local/bin/ /opt/homebrew/bin/
 
 set -x EDITOR /usr/bin/env nvim
+set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
 set -x GPG_TTY (tty)
 
 if status is-interactive
