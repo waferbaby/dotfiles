ac1aa3f80ac2e8939dc5cec1731fb3d445201d58
Daniel Bogan <d@waferbaby.com>
2023-08-07 13:46:12 +1000

    Remove SQL

--- source/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ source/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	init = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "css", "fish", "html", "javascript", "json", "lua", "markdown", "ruby", "sql" },
+			ensure_installed = { "css", "fish", "html", "javascript", "json", "lua", "markdown", "ruby" },
 			auto_install = true,
 			highlight = {
 				enable = true,
