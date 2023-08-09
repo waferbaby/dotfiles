6ead5b2a62f7308059be9d3005e9ab0b29a7740a
Daniel Bogan <d@waferbaby.com>
2023-08-09 10:53:14 +1000

    Remove incline

--- source/.config/nvim/lua/plugins/incline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"b0o/incline.nvim",
-	opts = {
-		render = function(props)
-			local bufname = vim.api.nvim_buf_get_name(props.buf)
-			return bufname ~= "" and vim.fn.fnamemodify(bufname, ":p") or "..."
-		end,
-	},
-}
