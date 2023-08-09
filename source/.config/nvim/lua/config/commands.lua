bad61c8655e45dd5c65857a88105924a065e71e0
Daniel Bogan <d@waferbaby.com>
2023-08-09 10:55:19 +1000

    Only lint on file write

--- source/.config/nvim/lua/config/commands.lua
+++ source/.config/nvim/lua/config/commands.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "silent! cd %:p:h" })
 vim.api.nvim_create_autocmd("BufWritePost", { pattern = "*", command = "FormatWrite" })
-vim.api.nvim_create_autocmd("BufModifiedSet", {
+vim.api.nvim_create_autocmd("BufWritePost", {
 	callback = function()
 		require("lint").try_lint()
 	end,
