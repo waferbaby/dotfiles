d9f80449091380f25b4d283259acd2912a61fcd8
Daniel Bogan <d@waferbaby.com>
2023-08-13 07:45:23 +0000

    Remove the gear function

--- source/.config/fish/functions/gear.fish
+++
@@ -1,5 +0,0 @@
-function gear
-  cd ~/dev/usesthis/gear
-  fzf -e --preview="cat {}" --preview-window=top:60%:wrap --bind "enter:execute-silent(basename {} | sed 's/.json//' | tr -d '\n' | pbcopy)"
-  cd ~/dev/usesthis
-end
