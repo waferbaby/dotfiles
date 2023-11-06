8395be5e465cb448886e6f20490fba6a0dfb142e
Daniel Bogan <d@waferbaby.com>
2023-11-06 12:54:11 +1100

    Remove the lint/format autocommands

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,9 +1,2 @@
 vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "silent! cd %:p:h" })
-vim.api.nvim_create_autocmd("BufWritePost", { pattern = "*", command = "FormatWrite" })
-vim.api.nvim_create_autocmd("BufWritePost", {
-	callback = function()
-		require("lint").try_lint()
-	end,
-})
-
 vim.api.nvim_create_user_command("Rg", "silent! grep <q-args> | copen", { nargs = "*" })
