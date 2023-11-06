04608e6fc9eee68407b693eca65351b01830efcf
Daniel Bogan <d@waferbaby.com>
2023-11-06 16:00:21 +1100

    Remove Rubocop setup

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,8 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	init = function()
     local lspconfig = require('lspconfig')
-
-    lspconfig.rubocop.setup({})
+    
     lspconfig.standardrb.setup({})
 	end,
 }
