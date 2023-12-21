a8b408198dbdc29ef96dc0e8afbeef389f61f55c
Daniel Bogan <d@waferbaby.com>
2023-12-22 10:13:16 +1100

    Tweak the default configs

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,22 +1,30 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = function()
-		return {
-			defaults = {
-				preview = false,
-				prompt_position = "bottom",
-				prompt_title = false,
-				mappings = {
-					i = { ["<esc>"] = require("telescope.actions").close },
-				},
-			},
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = function()
+    return {
+      defaults = {
+        preview = false,
+        prompt_position = "bottom",
+        prompt_title = false,
+        mappings = {
+          i = { ["<esc>"] = require("telescope.actions").close },
+        },
 
-			pickers = {
-				find_files = { theme = "dropdown" },
-				git_status = { theme = "dropdown" },
-				buffers = { theme = "dropdown" },
-			},
-		}
-	end,
+        sorting_strategy = "ascending",
+        border = true,
+        borderchars = { "─", "", "", "", "", "", "", "" },
+        layout_strategy = "bottom_pane",
+        layout_config = {
+          height = 8,
+        },
+      },
+
+      pickers = {
+        find_files = { theme = "dropdown" },
+        git_status = { theme = "dropdown" },
+        buffers = { theme = "dropdown" },
+      },
+    }
+  end,
 }
