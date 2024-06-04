52cc358525aedbbfe02d622b41f7037a57e59da6
Daniel Bogan <d@waferbaby.com>
2024-06-04 18:34:18 +1000

    Remove the theme

--- .config/nvim/lua/plugins/current_theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "baliestri/aura-theme",
-  lazy = false,
-  priority = 1000,
-  config = function(plugin)
-    vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-    vim.cmd([[colorscheme aura-dark]])
-  end
-}
