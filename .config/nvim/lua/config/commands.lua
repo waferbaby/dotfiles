7309b26571c9438770bd2111c42d041eed14b55b
Daniel Bogan <d@waferbaby.com>
2024-08-13 11:41:34 +1000

    Remove the hand-rolled LSP setup

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,33 +1,3 @@
 vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "silent! cd %:p:h" })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'ruby',
-  callback = function(args)
-    vim.lsp.start({
-      name = 'rubocop',
-      cmd = { 'bundle', 'exec', 'rubocop', '--lsp' },
-      root_dir = vim.fs.root(args.buf, { 'Gemfile', '.git' }),
-    })
-  end,
-})
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-    if client.supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end
-
-    if client.supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({bufnr = args.buf, id = client.id})
-        end,
-      })
-    end
-  end,
-})
-
 vim.api.nvim_create_user_command("Rg", "silent! grep <q-args> | copen", { nargs = "*" })
