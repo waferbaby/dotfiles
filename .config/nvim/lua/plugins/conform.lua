0ea3cd74f7d0de422d07707fb0d431f760cac676
Daniel Bogan <d@waferbaby.com>
2024-09-10 09:48:37 +1000

    Add JSON linting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,17 +1,18 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = function()
-    return {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        ruby = { "rubocop" },
-      },
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = function()
+		return {
+			formatters_by_ft = {
+				json = { "jq" },
+				lua = { "stylua" },
+				ruby = { "rubocop" },
+			},
 
-      format_on_save = {
-        timeout_ms = 1500,
-        lsp_fallback = true,
-      },
-    }
-  end,
+			format_on_save = {
+				timeout_ms = 1500,
+				lsp_fallback = true,
+			},
+		}
+	end,
 }
--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,17 +1,18 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile", },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      ruby = { "rubocop" },
-    }
+		lint.linters_by_ft = {
+			json = { "jq" },
+			ruby = { "rubocop" },
+		}
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-  end,
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
