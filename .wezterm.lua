aacc5b0197c51cb9bd4deec3df1939c08e9321db
Daniel Bogan <d@waferbaby.com>
2024-11-07 18:14:16 +1100

    Disable the default Wez control-shift-arrow keys

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,5 +3,31 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Molokai"
 config.enable_tab_bar = false
+config.font_size = 14
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+
+	{
+		key = "RightArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+
+	{
+		key = "UpArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+
+	{
+		key = "DownArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 return config
