198347e6ab9e3be8405bf5b0c4bfbc05095ab880
Daniel Bogan <d@waferbaby.com>
2025-12-03 16:44:30 +1100

    Tweak the options

--- .config/nvim/lsp/ruby.lua
+++ .config/nvim/lsp/ruby.lua
@@ -1,7 +1,16 @@
 return {
   vim.lsp.config('ruby', {
-    root_markers = { '.git' },
+    root_markers = { 'Gemfile', '.git' },
     cmd = { 'ruby-lsp' },
     filetypes = { 'ruby', 'eruby' },
+    init_options = {
+      formatter = 'standard',
+      linters = { 'standard' },
+      addonSettings = {
+        ["Ruby LSP Rails"] = {
+          enablePendingMigrationsPrompt = false,
+        },
+      },
+    }
   })
 }
