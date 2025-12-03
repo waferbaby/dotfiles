6359404c406beb69236ada6af59a6c55998ae3a7
Daniel Bogan <d@waferbaby.com>
2025-12-03 16:44:41 +1100

    Remove the format command

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,10 +1,3 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format({ async = true })
-  end
-})
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
