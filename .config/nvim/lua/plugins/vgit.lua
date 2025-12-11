de8da93ce1ad4c55c163978a037a1780bc78d4bd
Daniel Bogan <d@waferbaby.com>
2025-12-11 14:23:07 +1100

    Remove vgit.lua

--- .config/nvim/lua/plugins/vgit.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'tanvirtin/vgit.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
-  event = 'VimEnter',
-  config = function() require("vgit").setup() end,
-}
