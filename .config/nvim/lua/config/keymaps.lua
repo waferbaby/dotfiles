0ab18b0d343a912f73dc96c20abb46238a4e2fb4
Daniel Bogan <d@waferbaby.com>
2025-12-11 14:23:39 +1100

    Switch hunk jump to gitsigns

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,33 +13,16 @@ vim.keymap.set("n", "<leader>g", function()
   require("telescope.builtin").git_status({})
 end)
 
-vim.keymap.set("n", "<leader>t", function()
-	require("telescope.builtin").find_files({})
-end)
+vim.keymap.set("n", "<leader>t", function() require("telescope.builtin").find_files({}) end)
 
 -- Navigator
 
-vim.keymap.set("n", "<C-Down>", function()
-	require("Navigator").down()
-end)
-vim.keymap.set("n", "<C-Left>", function()
-	require("Navigator").left()
-end)
-vim.keymap.set("n", "<C-Right>", function()
-	require("Navigator").right()
-end)
-vim.keymap.set("n", "<C-Up>", function()
-	require("Navigator").up()
-end)
-
--- VGit
-
-vim.keymap.set("n", "<leader><leader>", function()
-	require("vgit").hunk_down()
-end)
+vim.keymap.set("n", "<C-Down>", function() require("Navigator").down() end)
+vim.keymap.set("n", "<C-Left>", function() require("Navigator").left() end)
+vim.keymap.set("n", "<C-Right>", function() require("Navigator").right() end)
+vim.keymap.set("n", "<C-Up>", function() require("Navigator").up() end)
 
 -- Gitsigns
 
-vim.keymap.set("n", "gb", function()
-	require("gitsigns").blame_line({})
-end)
+vim.keymap.set("n", "gb", function() require("gitsigns").blame_line({}) end)
+vim.keymap.set("n", "<leader><leader>", function() require("gitsigns").next_hunk() end)
