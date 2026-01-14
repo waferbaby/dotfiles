ecc0ac3a58ee654f7c0ea9a54deadb51dff63b53
Daniel Bogan <daniel.bogan@getmosh.com>
2026-01-15 10:04:54 +1100

    Only look at .rb files

--- .config/nvim/lsp/ruby.lua
+++ .config/nvim/lsp/ruby.lua
@@ -1,16 +1,16 @@
 return {
-  vim.lsp.config('ruby', {
-    root_markers = { 'Gemfile', '.git' },
-    cmd = { 'ruby-lsp' },
-    filetypes = { 'ruby', 'eruby' },
-    init_options = {
-      formatter = 'standard',
-      linters = { 'standard' },
-      addonSettings = {
-        ["Ruby LSP Rails"] = {
-          enablePendingMigrationsPrompt = false,
-        },
-      },
-    }
-  })
+	vim.lsp.config("ruby", {
+		root_markers = { "Gemfile", ".git" },
+		cmd = { "ruby-lsp" },
+		filetypes = { "ruby" },
+		init_options = {
+			formatter = "standard",
+			linters = { "standard" },
+			addonSettings = {
+				["Ruby LSP Rails"] = {
+					enablePendingMigrationsPrompt = false,
+				},
+			},
+		},
+	}),
 }
